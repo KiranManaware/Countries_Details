bf24c1a348940c4bf50c03d23f18b1949d8a7a21
KiranManaware <kiranmanaware123@gamil.com>
2025-04-08 22:31:51 +0530

    dotenv

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const cors=require('cors')
 const app=express();
-const PORT=2000;
+require('dotenv').config();
+const PORT=process.env.PORT;
 
 //  Use CORS Middleware
 app.use(cors());
